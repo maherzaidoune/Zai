1722091b9db983de5fbfe19b74110f26f75e5488
Maher Zaydoun <maher.zaydoun@satoripop.com>
2020-06-10 00:34:58 +0100

    working on lexer : add initial data  values

--- src/lexer/lexer.js
+++ src/lexer/lexer.js
@@ -1,6 +1,11 @@
 export class Lexer {
+
   constructor(input) {
     this.input = input;
+    this.size = input && input.length;
+    this.buffer = [];
+    this.position = 0;
+    this.line = 0;
   }
 
   nextToken() {
